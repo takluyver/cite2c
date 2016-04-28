cd69da0cbd6b0523b901676a64633ede32b6da9a
Thomas Kluyver <thomas@kluyver.me.uk>
2016-04-28 17:40:08 +0100

    Rearrange into Python package

--- cite2c/install.py
+++ cite2c/install.py
@@ -4,8 +4,8 @@ from notebook.nbextensions import install_nbextension
 from notebook.services.config import ConfigManager
 
 print("Installing nbextension ...")
-c2cdir = pjoin(dirname(abspath(__file__)), 'cite2c')
-install_nbextension(c2cdir, user=True)
+c2cdir = pjoin(dirname(abspath(__file__)), 'nbext')
+install_nbextension(c2cdir, user=True, destination='cite2c')
 
 print("Enabling the extension ...")
 cm = ConfigManager()
